948de148ebe6d8e1ed3e0ee35e06c9b0fb20c4dc
kaixuan1995 <2534189166@qq.com>
2016-09-10 09:50:57 +0800

    增加Git测试包

---
+++ src/com/jxapq/git/test/GitTest.java
@@ -0,0 +1,13 @@
+package com.jxapq.git.test;
+
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Hello Git");
+	}
+
+}

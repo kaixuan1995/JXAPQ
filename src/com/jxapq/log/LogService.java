e64ca87f70a4dd4f9171c3d780d622cd761ab1bb
kaixuan1995 <2534189166@qq.com>
2016-09-15 09:31:59 +0800

    修改dev

--- src/com/jxapq/log/LogService.java
+++ src/com/jxapq/log/LogService.java
@@ -13,7 +13,7 @@ public class LogService {
 	String path = this.getClass().getClassLoader().getResource("/").getPath()
 			+ "com/jxapq/config/Log.xml";
 
-	//增加日志 测试git pull --rebase
+	//增加日志我是dev
 	public void addLog(String voName, String log_type, String user_id)
 			throws DBException {
 		Log log = ParseLogXML.parseLogXml(path, voName, log_type);
